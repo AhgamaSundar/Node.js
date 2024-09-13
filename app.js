7c42c4b6c8458b5e0d1e88d1f25a3c51ee31d92f
AhgamaSundar <ahgamasundar40@gmail.com>
2024-09-13 23:40:32 +0530

    error handling

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express=require('express');
+const uuid=require('uuid');
 
 const path=require('path');
 const fs=require('fs');
+const { render } = require('ejs');
 const app=express();
 
 app.set('views',path.join(__dirname,'views'));
@@ -17,6 +19,21 @@ app.get('/',function (req,res) {
     
     
 });
+app.get('/restaurants/:resId',function(req,res){
+    const restaurantID=req.params.resId;
+    const filePath=path.join(__dirname,'data','restaurants.json');
+    const fileData=fs.readFileSync(filePath);
+    const storedRestaurants=JSON.parse(fileData);
+    for(const rest of storedRestaurants){
+        if(rest.id===restaurantID){
+            return res.render('restaurants-Details',{restaurant:rest});
+
+        }
+    }
+    res.render('404.ejs')
+
+
+});
 app.get('/restaurants',function(req,res){
     const filePath=path.join(__dirname,'data','restaurants.json');
     const fileData=fs.readFileSync(filePath);
@@ -24,29 +41,34 @@ app.get('/restaurants',function(req,res){
     res.render('restaurants',{placeholder:storedRestaurants.length,restaurants:storedRestaurants});
     
 
-})
+});
 app.get('/about',function(req,res){
     res.render('about');
-})
+});
 app.get('/confirm',function(req,res){
     res.render('confirm');
 
-})
+});
 app.get('/index',function(req,res){
     res.render('index');
-})
+});
 app.get('/recommend',function(req,res){
     res.render('recommend');
 
-})
+});
 app.post('/recommend',function(req,res){
     const restaurant =req.body;
+    restaurant.id =uuid.v4();
     const filePath=path.join(__dirname,'data','restaurants.json');
     const fileData=fs.readFileSync(filePath);
     const storedRestaurants=JSON.parse(fileData);
     storedRestaurants.push(restaurant);
     fs.writeFileSync(filePath,JSON.stringify(storedRestaurants));
     res.redirect('/confirm');
+});
+
+app.use(function(error,req,res,next){
+    render(500);
 });
 app.listen(3000);
 
